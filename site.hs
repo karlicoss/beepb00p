3de7bba8d0789318656ce024a6860ba0204f2cce
Dima Gerasimov <karlicoss@gmail.com>
2019-01-09 08:26:34 +0000

    Attempt to import ipynb as markdown

--- site.hs
+++ site.hs
@@ -3,6 +3,8 @@
 import           Data.Monoid (mappend)
 import           Hakyll
 
+import System.FilePath (replaceExtension)
+
 import Text.Pandoc (readOrg, Pandoc(..), docTitle, docDate, Meta, Inline)
 import Text.Pandoc.Shared (stringify)
 import Text.Pandoc.Options (def, writerVariables, writerTableOfContents)
@@ -35,18 +37,55 @@ combineContexts (Context f) (Context g) = Context $ \k a Item{itemBody=PandocX p
 myContext :: Context PandocX
 myContext = combineContexts pandocContext defaultContext
 
-defaultDate = constField "date" "2019-09-01"
+defaultDate = constField "date" "2019-01-01"
 
 -- thanks to https://github.com/gibiansky/blog/blob/668a5bf7ae6815a20dd6d57c900318e34c959c13/Compilers.hs
 compileWithFilter :: String -> [String] -> Item String -> Compiler (Item String)
 compileWithFilter cmd args = withItemBody (unixFilter cmd args)
 
-ipynbPost :: Item String -> Compiler (Item String)
-ipynbPost = compileWithFilter command arguments
-  where command = "jupyter"
-        arguments = ["nbconvert", "--stdin", "--stdout"]
---
+-- TODO hmm, that should probably be pre commit hook for content git repo?
+-- wouldn't hurt to run again and check?
+-- python3 ./ipynb_output_filter.py <lagr.ipynb >lagr2.ipynb 
 
+ipynbFilterOutput :: Item String -> Compiler (Item String)
+ipynbFilterOutput = compileWithFilter cmd args
+  where cmd = "python3"
+        args = [ "/L/Dropbox/repos/ipynb_output_filter/ipynb_output_filter.py" ]
+
+ipynbRun :: Item String -> Compiler (Item String)
+ipynbRun = compileWithFilter command arguments
+  where command = "jupyter"
+        arguments = ["nbconvert"
+                    , "--execute"
+                    , "--to", "markdown"
+                    , "--stdin"
+                    , "--stdout"
+                    ]
+
+-- (<&>)  = flip fmap
+-- infixl 1 <&>
+
+
+renameItem :: (String -> String) -> Item a -> Item a
+renameItem f i =  i { itemIdentifier = new_id } where
+  old_id = itemIdentifier i
+  old_version = identifierVersion old_id
+  new_id = setVersion old_version $ (fromFilePath $ f $ toFilePath old_id)
+
+-- TODO ugh itemIdentifier is not exported???
+-- renameItem f x = x { itemIdentifier = new_id } where
+--   old_id = itemIdentifier x -- TODO do I need lens?..
+--   old_path = identifierPath old_id
+--   new_path = f old_path
+--   new_id = old_id { identifierPath = new_path }
+
+ipynbCompile i = do
+  ipy <- ipynbRun i  -- x <&> (renameItem (\f -> replaceExtension f ".md")) -- ipynbFilterOutput >> ipynbRun
+  let ipy_md = renameItem (\f -> replaceExtension f ".md") ipy -- change the extension to trick pandoc...
+  pandoc <- readPandoc ipy_md
+  let html = writePandoc pandoc
+  let res = renameItem (\f -> replaceExtension f ".ipynb") html 
+  return res
 
 --------------------------------------------------------------------------------
 main :: IO ()
@@ -92,10 +131,17 @@ main = hakyll $ do
     -- TODO posts/etc is lame, use top level
     -- TODO rss
     -- TODO tags would be nice...
-    match "posts/**.ipynb" $ do
+    match "content/**.ipynb" $ do
         route $ setExtension "html"
-        compile $ do
-          getResourceString >>= ipynbPost
+        compile $ getResourceString
+              >>= ipynbCompile
+              >>= loadAndApplyTemplate "templates/post.html"    postCtx
+              >>= loadAndApplyTemplate "templates/default.html" postCtx
+              >>= relativizeUrls
+
+          -- >>= readPandoc <&> writePandoc
+          -- >>= writePandoc
+          -- >>= loadAndApplyTemplate "templates/post.html"    postCtx
           -- TODO pandocCompiler
           --   >>= loadAndApplyTemplate "templates/post.html"    postCtx
 
@@ -132,7 +178,7 @@ main = hakyll $ do
         route   $ gsubRoute "meta/" (const "")
         compile $ do
             -- TODO extract in a variable or something
-            posts <- loadAll "content/**.md" -- recentFirst =<< loadAll "posts/**.md" -- TODO recentFirst -- TODO actually, list all..
+            posts <- loadAll ("content/**.md" .||. "content/**.ipynb") -- recentFirst =<< loadAll "posts/**.md" -- TODO recentFirst -- TODO actually, list all..
             let indexCtx =
                     listField "posts" postCtx (return posts)
                     <> constField "title" "Home"
