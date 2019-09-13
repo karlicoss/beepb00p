84e5f75caa6894d8a2c66882f1c775bf44f606b3
Dima Gerasimov <karlicoss@gmail.com>
2019-09-13 22:25:43 +0300

    fix template in ipynb config

--- misc/ipynbconfig.py
+++ misc/ipynbconfig.py
@@ -1,7 +1,16 @@
+# good reference: https://github.com/jupyter/jupyter_core/blob/master/jupyter_core/tests/dotipython_empty/profile_default/ipython_nbconvert_config.py
+
 c = get_config()
 c.NbConvertApp.export_format = 'html'
-c.NbConvertApp.template = 'misc/mybasic.tpl'
 
-c.TagRemovePreprocessor.remove_cell_tags = {'noexport'} #  TODO emacs tags?
+c.HTMLExporter.template_file = 'misc/mybasic.tpl'
+
+c.TagRemovePreprocessor.remove_cell_tags.add('noexport') #  TODO emacs tags?
 
 # c.Exporter.preprocessors.append('nbconvert.preprocessors.extractoutput.ExtractOutputPreprocessor')
+
+# TODO
+# c.NbConvertApp.output_files_dir = 'NoCodeHtml'
+# c.TagRemovePreprocessor.remove_input_tags.add("script")
+# c.TagRemovePreprocessor.remove_input_tags.add("js_code")
+
