a1c976396be6d99a8fc724415012eac5d1e1606a
Dima Gerasimov <karlicoss@gmail.com>
2019-09-15 11:43:07 +0400

    clean up ipynb compiling; remove output filter

--- Ipynb.hs
+++ Ipynb.hs
@@ -19,11 +19,6 @@ import Debug.Trace (trace)
 stripPrivateTodos :: Item String -> Compiler (Item String)
 stripPrivateTodos = compileWithFilter "grep" ["-v", "NOEXPORT"]
 
-ipynbFilterOutput :: Item String -> Compiler (Item String)
-ipynbFilterOutput = compileWithFilter cmd args
-  where cmd = "python3"
-        args = [ "/L/soft/ipynb_output_filter/ipynb_output_filter.py" ] -- TODO use nbstripout??
-
 
 -- ugh, images do not really work in markdown..
 -- , "--to", "markdown"
@@ -39,6 +34,8 @@ ipynbRun item = do
   res <- compileWithFilter "misc/compile-ipynb" ["--output-dir", "_site", "--item", iid] item
   return res
 
+ipynbCompile = stripPrivateTodos >=> ipynbRun
+ipynbCompiler = getResourceString >>= ipynbCompile
 
 -- renameItem :: (String -> String) -> Item a -> Item a
 -- renameItem f i =  i { itemIdentifier = new_id } where
@@ -53,7 +50,6 @@ ipynbRun item = do
 --   new_path = f old_path
 --   new_id = old_id { identifierPath = new_path }
 
-ipynbCompile = stripPrivateTodos >=> ipynbFilterOutput >=> ipynbRun
   -- ipy <- ipynbRun i  -- x <&> (renameItem (\f -> replaceExtension f ".md")) -- ipynbFilterOutput >> ipynbRun
   -- let ipy_md = renameItem (\f -> replaceExtension f ".md") ipy -- change the extension to trick pandoc...
   -- pandoc <- readPandoc ipy_md
@@ -63,4 +59,3 @@ ipynbCompile = stripPrivateTodos >=> ipynbFilterOutput >=> ipynbRun
 
 -- TODO release ipython stuff in a separate file so it's easy to share
 
-ipynbCompiler = getResourceString >>= ipynbCompile
