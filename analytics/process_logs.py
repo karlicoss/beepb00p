7ab1c9efe758644bb1ef4d86ccdbd08d2e7cbdbf
Dima Gerasimov <karlicoss@gmail.com>
2019-12-01 18:34:24 +0000

    parse date, --html mode

--- analytics/process_logs.py
+++ analytics/process_logs.py
@@ -1,13 +1,20 @@
+from datetime import datetime
 from pathlib import Path
 import re
 
 
-LINEFORMAT = re.compile( r"""(?P<ipaddress>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - (?P<remoteuser>.+) \[(?P<dateandtime>\d{2}\/[a-z]{3}\/\d{4}:\d{2}:\d{2}:\d{2} (\+|\-)\d{4})\] ((\"(?P<method>.+) )(?P<url>.+)(http\/[1-2]\.[0-9]")) (?P<statuscode>\d{3}) (?P<bytessent>\d+) (["](?P<refferer>(\-)|(.+))["]) (["](?P<useragent>.+)["])""", re.IGNORECASE)
 
+LINEFORMAT = re.compile( r"""(?P<ipaddress>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - (?P<remoteuser>.+) \[(?P<dt>\d{2}\/[a-z]{3}\/\d{4}:\d{2}:\d{2}:\d{2} (\+|\-)\d{4})\] ((\"(?P<method>.+) )(?P<url>.+)(http\/[1-2]\.[0-9]")) (?P<statuscode>\d{3}) (?P<bytessent>\d+) (["](?P<refferer>(\-)|(.+))["]) (["](?P<useragent>.+)["])""", re.IGNORECASE)
+
+DTFORMAT = '%d/%b/%Y:%H:%M:%S %z'
 
 def parse_line(line: str):
     zz = LINEFORMAT.search(line)
-    return zz.groupdict() if zz else {'error': line}
+    if zz is None:
+        return {'error': line}
+    dd = {**zz.groupdict()}
+    dd['dt'] = datetime.strptime(dd['dt'], DTFORMAT) # type: ignore
+    return dd
 
 
 def iter_log(access_log):
@@ -18,5 +25,8 @@ def iter_log(access_log):
 
 def get_dataframe(access_log):
     import pandas as pd # type: ignore
-    return pd.DataFrame(iter_log(access_log))
+    from itertools import islice
+    it = iter_log(access_log)
+    # it = islice(it, 5000)
+    return pd.DataFrame(it)
     # TODO filter errors here?
