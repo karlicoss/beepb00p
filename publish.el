cf4996a02c806485b7cf2cd04036f0a303ebac9b
Dima Gerasimov <karlicoss@gmail.com>
2019-12-22 21:14:22 +0000

    hack few things to build table of contents correctly

--- publish.el
+++ publish.el
@@ -1,41 +1,42 @@
 (require 'org)
+(require 'subr-x)
+
 ; TODO fucking hell, it doesn't seem capable of resolving symlinks
-(defun my-org-list-to-org (list &optional params)
-  "my list to org"
-  (let* ((make-item
-          (lambda (type _depth &optional c)
-            (concat (if (eq type 'ordered) "1. " "* ")
-                    (and c (format "[@%d] " c)))))
-         (defaults
-           (list :istart make-item
-                 :icount make-item
-                 :ifmt (lambda (_type contents)
-                         (replace-regexp-in-string "\n" "\n  " contents))
-                 :dtend " :: "
-                 :cbon "[X] "
-                 :cboff "[ ] "
-                 :cbtrans "[-] ")))
-    (org-list-to-generic list (org-combine-plists defaults params))))
-(defun my-org-publish-sitemap (title list)
-  "my render sitemap"
-  (concat "#+TITLE: " title "\n\n"
-          (my-org-list-to-org list)))
+
+
+;; examples of sitemap formatting
+;; https://github.com/nanjj/nanjj.github.io/blob/4338fa60b07788885d3d4c8b2c684360a67e8098/org-publish.org
+
+
+(defun my/org-publish-sitemap-entry (entry style project)
+  ;; mdbook doesn't like list item not being a link
+  ;; and default sitemap entry function explicitly ignores directories
+  (if (directory-name-p entry)
+
+      ;; README.md got some special handling, so we abuse that
+      ;; https://rust-lang.github.io/mdBook/format/config.html?highlight=readme#configuring-preprocessors
+      (format "[[file:%sREADME.org][%s]]" (directory-file-name entry) (directory-file-name entry))
+      (org-publish-sitemap-default-entry entry style project)))
 
 (setq
  org-publish-project-alist
  '(("blog"
     :base-directory "content"
     :base-extension "org"
-    ; :makeindex
-    ; :auto-index t
-    :auto-sitemap t
-    :sitemap-filename "SUMMARY.org"
-    ;; :sitemap-function my-org-publish-sitemap
-    ; :index-filename "sitemap.org"
-    ; :index-title "Sitemap"
     :publishing-directory "public/blog"
     :recursive t
     :publishing-function org-md-publish-to-md
+
+    :auto-sitemap t
+    :sitemap-format-entry my/org-publish-sitemap-entry
+    :sitemap-filename "SUMMARY.org"
+
+
+    ;; :makeindex
+    ;; :auto-index t
+    ; :index-filename "sitemap.org"
+    ; :index-title "Sitemap"
+
     :exclude "org.org")
    ("website" :components ("blog")))) ; TODO????
 
