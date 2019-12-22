57043bd022fd8975c673357c20a413e534c4f052
Dima Gerasimov <karlicoss@gmail.com>
2019-12-22 19:53:00 +0000

    adjustments for rendering with mdbook

--- publish.el
+++ publish.el
@@ -1,16 +1,41 @@
 (require 'org)
 ; TODO fucking hell, it doesn't seem capable of resolving symlinks
+(defun my-org-list-to-org (list &optional params)
+  "my list to org"
+  (let* ((make-item
+          (lambda (type _depth &optional c)
+            (concat (if (eq type 'ordered) "1. " "* ")
+                    (and c (format "[@%d] " c)))))
+         (defaults
+           (list :istart make-item
+                 :icount make-item
+                 :ifmt (lambda (_type contents)
+                         (replace-regexp-in-string "\n" "\n  " contents))
+                 :dtend " :: "
+                 :cbon "[X] "
+                 :cboff "[ ] "
+                 :cbtrans "[-] ")))
+    (org-list-to-generic list (org-combine-plists defaults params))))
+(defun my-org-publish-sitemap (title list)
+  "my render sitemap"
+  (concat "#+TITLE: " title "\n\n"
+          (my-org-list-to-org list)))
+
 (setq
  org-publish-project-alist
  '(("blog"
-    :base-directory "content/misc"
+    :base-directory "content"
     :base-extension "org"
-    :auto-index t
-    :index-filename "sitemap.org"
-    :index-title "Sitemap"
+    ; :makeindex
+    ; :auto-index t
+    :auto-sitemap t
+    :sitemap-filename "SUMMARY.org"
+    ;; :sitemap-function my-org-publish-sitemap
+    ; :index-filename "sitemap.org"
+    ; :index-title "Sitemap"
     :publishing-directory "public/blog"
     :recursive t
-    :publishing-function org-html-publish-to-html
+    :publishing-function org-md-publish-to-md
     :exclude "org.org")
    ("website" :components ("blog")))) ; TODO????
 
